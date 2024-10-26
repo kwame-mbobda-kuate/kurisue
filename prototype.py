933550f9f53759837e6eeafa7c6244aaf9125cb4
kmbobdakuate <kwame.mbobda-kuate@ensae.fr>
2024-10-26 12:45:31 +0000

    Completion of the prototype

--- prototype.py
+++ prototype.py
@@ -1,23 +1,27 @@
 import numpy as np
 import networkx as nx
-from scipy i
+import scipy
 import collections
+import warnings
+
+warnings.filterwarnings("ignore")
 
 UE = 0
 SO = 1
 
-def get_linear_function(a, b):
+Function = collections.namedtuple("Function", ["antideriv", "main", "deriv"])
 
-    def int(x):
+def get_linear_function(a: float, b: float):
+    def antideriv(x):
         return a*x**2 / 2 + b * x
     
     def main(x):
         return a*x + b
 
-    def derivative(x):
+    def deriv(x):
         return a
 
-    return collections.namedtuple("Function (linear)", function = main, derivative = derivative)
+    return  Function(antideriv, main, deriv)
 
 null_function = get_linear_function(0, 0)
 
@@ -27,51 +31,62 @@ class NetworkGraph:
         self.graph = graph
         self.od_demand = od_demand
         self.cost_functions = cost_functions
-        self.order = graph.order
+        self.order = graph.order()
 
     def solve(self, mode, max_steps = 20):
         if mode == UE:
             t = {edge: self.cost_functions[edge].main(0) for edge in G.edges}
-            shortest_path = nx.shortest_path(self.graph, weight = lambda i, j, ...: t[(i, j)])
-            x = np.zeros((order, order))
-            for (i, j) in G.edges:
-                path = shortest_path[i][j]
-                for k in range(len(path) - 1):
-                    x[path[k]][path[k+1]] += self.od_demand[i, j]
+            shortest_path = dict(nx.shortest_path(self.graph, weight = lambda i, j, data: t.get((i, j), 0)))
+            x = np.zeros((self.order, self.order))
+            for i in shortest_path:
+                for j in shortest_path[i]:
+                    path = shortest_path[i][j]
+                    for k in range(len(path) - 1):
+                        x[path[k]][path[k+1]] += self.od_demand[i, j]
         if mode == SO:
             ...
         for k in range(max_steps):
-            t = [self.cost_functions[edge].main(x[*edge]) for edge in G.edges]
-            shortest_path = nx.shortest_path(self.graph, weight = lambda i, j, ...: t[i, j])
-            y = np.zeros((order, order))
-            for (i, j) in G.edges:
-                path = shortest_path[i][j]
-                for k in range(len(path) - 1):
-                    y[path[k]][path[k+1]] += self.od_demand[i, j]
+            t = {edge: self.cost_functions[edge].main(x[*edge]) for edge in G.edges}
+            shortest_path = dict(nx.shortest_path(self.graph, weight = lambda i, j, data: t.get((i, j), 0)))
+            y = np.zeros((self.order, self.order))
+            for i in shortest_path:
+                for j in shortest_path[i]:
+                    path = shortest_path[i][j]
+                    for k in range(len(path) - 1):
+                        y[path[k]][path[k+1]] += self.od_demand[i, j]
 
             def f(alpha):
                 z = x + alpha * (y - x)
-                return np.sum(self.cost_functions[edge].int(z[edge]) for edge in self.graph.edges)
+                return sum(self.cost_functions[edge].antideriv(z[edge]) for edge in self.graph.edges)
 
             def f_p(alpha):
                 z = x + alpha * (y - x)
-                return np.sum((y[edge] - x[edge]) * self.cost_functions[edge].main(z[edge]) for edge in self.graph.edges)
+                return sum((y[edge] - x[edge]) * self.cost_functions[edge].main(z[edge]) for edge in self.graph.edges)
 
             res = scipy.optimize.minimize_scalar(f, bounds = (0, 1))
             alpha = res.x
             x = x + alpha * (y - x)
         return x
 
-G = nx.graph()
-G.add_edges_from([(0, 1), (0, 2), (1, 3), (2, 3)])
+G = nx.Graph()
+start = 0
+a = 1
+b = 2
+end = 3
+G.add_edges_from([(start, a), (start, b), (a, end), (b, end)])
 od_demand = np.zeros((4, 4))
-od_demand[0, 3] = 4000
+od_demand[start, end] = 4000
 cost_functions = {
-    (0, 1) : get_linear_function(1/100, 0),
-    (0, 2) : get_linear_function(0, 45),
-    (1, 3) : get_linear_function(0, 45),
-    (0, 2) : get_linear_function(1/100, 0)
+    (start, a) : get_linear_function(1/100, 0),
+    (start, b) : get_linear_function(0, 45),
+    (a, end) : get_linear_function(0, 45),
+    (b, end) : get_linear_function(1/100, 0)
                  }
 
-network_graph = NetworkGraph(G, od_demand, cost_function)
+network_graph = NetworkGraph(G, od_demand, cost_functions)
+print(network_graph.solve(UE))
+
+G.add_edge(a, b)
+cost_functions[(a, b)] = null_function
+network_graph = NetworkGraph(G, od_demand, cost_functions)
 print(network_graph.solve(UE))
