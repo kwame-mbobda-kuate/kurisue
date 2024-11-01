a7d8bb89340d070894a0d61ba8fe22b53f63d91c
kmbobdakuate <kwame.mbobda-kuate@ensae.fr>
2024-11-01 21:22:33 +0000

    Writing  of the code for the downloading and the cleaning of the datasets

---
+++ data_management.py
@@ -0,0 +1,120 @@
+import requests
+import os
+import subprocess
+import shutil
+from tqdm import tqdm
+import numpy
+import osmnx
+import numpy as np
+import pandas
+
+MILE_IN_METER = 1609.344
+MAX_SPEED = 55 * MILE_IN_METER
+MIN_LON, MIN_LAT, MAX_LON, MAX_LAT = -74.2549337, 40.4983853, -73.7004728, 40.912507
+
+
+graph = osmnx.graph.graph_from_place("New York", network_type="drive")
+
+join = os.path.join
+
+
+def download_url_with_bar(url: str, fname: str, chunk_size=1024):
+    resp = requests.get(url, stream=True)
+    total = int(resp.headers.get("content-length", 0))
+    print(os.getcwd())
+    with open(fname, "wb") as file, tqdm(
+        desc=fname,
+        total=total,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=chunk_size):
+            size = file.write(data)
+            bar.update(size)
+
+
+def download_datasets(data_path: str):
+    subprocess.run(["sudo", "apt-get", "install", "fastjar"])
+    os.chdir(data_path)
+    foil_path = "FOIL2013"
+    foil_zip_path = join(foil_path, "FOIL2013.zip")
+    os.makedirs(foil_path, exist_ok=True)
+    download_url_with_bar(
+        "https://databank.illinois.edu/datafiles/ws2jn/download", foil_zip_path
+    )
+    subprocess.run(["jar", "xvf", foil_zip_path])
+    shutil.rmtree("__MACOSX")
+    os.chdir("FOIL2013")
+    for i in range(1, 13):
+        subprocess.run(["jar", "xvf", f"trip_data_{i}.zip"])
+        os.remove(f"trip_data_{i}.zip")
+        os.remove(f"trip_fare_{i}.zip")
+    os.remove("FOIL2013.zip")
+    os.chdir("..")
+
+    download_url_with_bar(
+        "https://data.cityofnewyork.us/api/views/7ym2-wayt/rows.csv",
+        "automated_traffic_volume_counts.csv",
+    )
+
+
+def clean_datasets(foil_path: str):
+
+    for i in range(1, 13):
+        df = pandas.read_csv(os.path.join(foil_path, f"trip_data_{i}.csv"))
+        os.remove(os.path.join(foil_path, f"trip_data_{i}.csv"))
+        df.drop(
+            [
+                "medallion",
+                " hack_license",
+                " vendor_id",
+                " rate_code",
+                " store_and_fwd_flag",
+            ],
+            axis=1,
+            inplace=True,
+        )
+        df.rename(columns={x: x.strip() for x in df.columns}, inplace=True)
+
+        mask_long_max = df["pickup_longitude"] <= MAX_LON
+        mask_long_min = df["pickup_longitude"] >= MIN_LON
+        mask_lat_max = df["pickup_latitude"] <= MAX_LAT
+        mask_lat_min = df["pickup_latitude"] >= MIN_LAT
+        df = df[
+            mask_lat_max & mask_lat_min & mask_long_max & mask_long_min
+        ]  # The trip must start and end in New York
+
+        df = df[
+            df["passenger_count"] <= 5
+        ]  # A taxi cannot accept more than 5 passengers
+
+        df["trip_distance"] = df["trip_distance"] * MILE_IN_METER
+        df = df[
+            df["trip_distance"] / df["trip_time_in_secs"] <= MAX_SPEED
+        ]  # The averegae speed  must be less than the maximum speed
+
+        df = df[
+            df["trip_time_in_secs"] <= 12 * 60 * 60
+        ]  # A trip cannot last more than 12 hours
+        df = df[
+            df["trip_distance"]
+            >= osmnx.distance.euclidean(
+                df["pickup_latitude"],
+                df["pickup_longitude"],
+                df["dropoff_latitude"],
+                df["dropoff_longitude"],
+            )
+        ]
+
+        df["pickup_datetime"] = pandas.to_datetime(df["pickup_datetime"])
+        df["dropoff_datetime"] = pandas.to_datetime(df["dropoff_datetime"])
+
+        duration = (df["dropoff_datetime"] - df["pickup_datetime"]).dt.total_seconds()
+        mask_chrono = duration > 0
+        mask_diff = np.abs(duration - df["trip_time_in_secs"]) <= 60
+        df = df[mask_chrono & mask_diff]
+
+        df = df.dropna()
+
+        df.to_parquet(os.path.join(foil_path, f"trip_data_{i}.parquet"))
---
+++ main.py
@@ -0,0 +1,4 @@
+import data_management
+
+data_management.download_datasets("data")
+data_management.clean_datasets("data/FOIL2013")
